610a14c150d58328b5751fc20c9fbd40db6c32b1
jailcomfranssa <jail_son_0@hotmail.com>
2023-01-06 19:35:40 -0300

    mapenado relacionamento ManyToMany entre Enrollment e Lesson e seeds

--- src/main/java/com/devsuperior/dslearnbds/model/entity/Enrollment.java
+++ src/main/java/com/devsuperior/dslearnbds/model/entity/Enrollment.java
@@ -4,8 +4,11 @@ import com.devsuperior.dslearnbds.model.pk.EnrollmentPK;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_enrollment")
@@ -17,6 +20,9 @@ public class Enrollment {
     private Instant refundMoment;
     private boolean available;
     private boolean onlyUpdate;
+
+    @ManyToMany(mappedBy = "enrollmentsDone")
+    private Set<Lesson> lessonsDone = new HashSet<>();
 
     public Enrollment() {
     }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -31,6 +31,21 @@ INSERT INTO tb_section(title,description,position,img_Uri,resource_id,prerequite
 INSERT INTO tb_enrollment(user_id,offer_id,enroll_Moment,refund_Moment,available,only_Update) values ( 1,1, '2020-07-14T10:00:00', null, true, false );
 INSERT INTO tb_enrollment(user_id,offer_id,enroll_Moment,refund_Moment,available,only_Update) values ( 2,1, '2020-07-14T10:00:00', null, true, false );
 
+INSERT INTO tb_lesson(title,position,section_id)values ('Aula-01 do capitolo 1', 1, 1);
+INSERT INTO tb_content(text_Content,videi_Uri,id)values ('Material de apoio ABC','https://wwww.video.com.br',1 );
+
+INSERT INTO tb_lesson(title,position,section_id)values ('Aula-02 do capitolo 1', 2, 1);
+INSERT INTO tb_content(text_Content,videi_Uri,id)values ('Material de apoio ABC','https://wwww.video.com.br',2 );
+
+INSERT INTO tb_lesson(title,position,section_id)values ('Aula-03 do capitolo 1', 3, 1);
+INSERT INTO tb_content(text_Content,videi_Uri,id)values ('','https://wwww.video.com.br',3);
+
+INSERT INTO tb_lesson(title,position,section_id)values ('Aula-03 do capitolo 1', 4, 1);
+INSERT INTO tb_task(id,description,question_Count,approval_Count,weight,due_Date)values (4,'Entregar tarefa',5,4,1.0,'2020-07-14T10:00:00' );
+
+insert INTO tb_lessons_done(lesson_id,user_id,offer_id)values ( 1,1, 1);
+insert INTO tb_lessons_done(lesson_id,user_id,offer_id)values ( 2,1, 1);
+
 
 
 
